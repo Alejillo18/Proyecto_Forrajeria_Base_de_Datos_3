40288aa7d422ebb82668b8200dd54726e81acdeb
Alejo Oviedo <alejoemmanueloviedo@gmail.com>
2026-06-17 17:51:38 -0300

    Subiendo cambios finales para Alejo

--- backend/src/modules/reportes/reportes.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { ReportesController } from './reportes.controller.js';
-import { verifyToken, checkRole } from '../../../middlewares/auth.middleware.js';
-
-const router = Router();
-
-router.get('/dashboard', verifyToken, checkRole(['Administrador']), ReportesController.getDashboard);
-
-export default router;
--- backend/src/modules/vendedores/vendedores.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { VendedoresController } from './vendedores.controller.js';
-
-const router = Router();
-router.get('/', VendedoresController.getAll);
-router.post('/', VendedoresController.create);
-router.put('/:id', VendedoresController.update);
-
-export default router;
